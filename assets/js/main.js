22cb21741a91b23c272ad84d57494521aa57ed6f
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-11 19:56:03 +0200

    erledigt

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,25 +3,36 @@ console.log('servus');
 
 
 // Level1_2
-function weather(quality) {
-    if (quality >= 8 && quality <= 10) {
+
+let wert = document.getElementById('wert');
+let quali = document.getElementById('quali');
+
+function weather() {
+    if (quali.value >= 8 && quali.value <= 10) {
         console.log('super');
-    } else if (quality >= 6 && quality <= 7) {
+        wert.innerHTML = "super";
+    } else if (quali.value >= 6 && quali.value <= 7) {
         console.log('guut');
-    } else if (quality >= 3 && quality <= 5) {
+        wert.innerHTML = "guut";
+    } else if (quali.value >= 3 && quali.value <= 5) {
         console.log('ookay');
-    } else if (quality >= 0 && quality <= 2) {
+        wert.innerHTML = "ookay";
+    } else if (quali.value >= 0 && quali.value <= 2) {
         console.log('schlecht');
+        wert.innerHTML = "schlecht";
     } else {
         console.log('Ergebnis kann nicht ausgewertet werden.');
+        wert.innerHTML = "Ergebnis kann nicht ausgewertet werden.";
     }
 }
 
-weather(5);
-weather(9);
-weather(2);
+// weather(5);
+// weather(9);
+// weather(2);
 
-//Level2_2
+
+
+//Level1_1
 let ergebnis = document.getElementById('ergebnis');
 let alter = document.getElementById('alter');
 
@@ -32,5 +43,20 @@ function adult() {
     } else if (alter.value < 18) {
         console.log('false');
         ergebnis.innerHTML = "minderjährig";
+    }
+}
+
+// Level1_3
+
+let input = document.getElementById('input');
+let output = document.getElementById('output');
+
+function greaterThan() {
+    if (input.value >= 18) {
+        output.innerHTML = "Ja. Du darfst Shisha rauchen."
+        console.log(true);
+    } else if (input.value < 18) {
+        output.innerHTML = "Nein. Du darfst keine Shisha rauchen."
+        console.log(false);
     }
 }
