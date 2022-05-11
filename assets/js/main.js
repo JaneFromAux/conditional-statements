c6b6b776d6a1c84e1074082c7c44d745f39e3c7b
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-11 17:44:03 +0200

    adult volljährig

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 
 console.log('servus');
 
+
+// Level1_2
 function weather(quality) {
     if (quality >= 8 && quality <= 10) {
         console.log('super');
@@ -13,9 +15,22 @@ function weather(quality) {
     } else {
         console.log('Ergebnis kann nicht ausgewertet werden.');
     }
-
 }
 
 weather(5);
 weather(9);
 weather(2);
+
+//Level2_2
+let ergebnis = document.getElementById('ergebnis');
+let alter = document.getElementById('alter');
+
+function adult() {
+    if (alter.value >= 18) {
+        console.log('true');
+        ergebnis.innerHTML = "volljährig";
+    } else if (alter.value < 18) {
+        console.log('false');
+        ergebnis.innerHTML = "minderjährig";
+    }
+}
